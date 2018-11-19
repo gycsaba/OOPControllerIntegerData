ee6cbc18263b517049e7dd3b1c705da39c2b696a
Csaba Gyuris <gycsaba@vasvari.hu>
2018-11-19 12:16:13 +0100

    Vezérlő osztály fájlból beolvasott egész számok kezelésére.

---
+++ OOPControllerIntegerData/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ControllerIntegerDataProcessing
+{
+    class Controller
+    {
+        List<int> subjects;
+        public Controller()
+        {
+            subjects = new List<Int32>();
+        }
+        public void readFromFile()
+        {
+            StreamReader sr = new StreamReader("adatok.txt", Encoding.UTF8);
+            while(!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                int a = Convert.ToInt32(line);
+                subjects.Add(a);
+            }
+        }
+        public int getNumberOfSubject()
+        {
+            return subjects.Count();
+        }
+    }
+}
---
+++ OOPControllerIntegerData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerIntegerDataProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller c = new Controller();
+            c.readFromFile();
+            Console.WriteLine("Beolvasott adatok száma: " + c.getNumberOfSubject());
+            Console.ReadKey();
+        }
+    }
+}
